8cd60b6f3379eb9087c02ed985d6753b8badbca3
saeed <mohammadsaeedmedia@gmail.com>
2021-07-16 01:55:52 +0430

    I created header and made it responsive

--- client/shadnak/src/component/Pages/firstpage/header.js
+++ client/shadnak/src/component/Pages/firstpage/header.js
@@ -1,39 +1,63 @@
-//import React , {useState} from 'react'
+/* eslint-disable no-restricted-globals */
+import { useState } from 'react';
 import {Link} from 'react-router-dom'
 import './styles.css'
 import logo from './../../../images/Logo-Shadnak.png' 
-//import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from './../../../images/menu-icon.png'
+import {useMediaQuery} from 'react-responsive';
+import  './styles.js';
+import { DesktopNav ,MobileNav,ShowMenu } from './styles.js'; //Maybe I shoud use dynamic redering for speed-up loading
 //import FetchData from './../../../api.js'
 //First I decided to use materil-ui but it was  messy and i didn't like it
-const Header = ()=>{
+const Header = ()=>{ 
+   const[open , setOpen]=useState(false)
+   const handleMediaQueryChange = ()=> {
+    if(isMobile === false && open === true){
+      setOpen(prev => prev = false)
+    }
+  }
+  const isMobile = useMediaQuery(
+    { maxDeviceWidth: 708 }, undefined,handleMediaQueryChange
+  );
+   const content = (
+    <header className='container' >
+    <img src ={logo} alt ='لوگو شادناک' className = 'logo' ></img>
+    <div className = 'break'></div>
+    <img  src = {MenuIcon} onClick={()=>{
+       setOpen(prev => !prev)
+       
+    }} className = 'icon' alt='فهرست'> 
+      </img>
     
-    return ( 
-      
-        <header className = 'container'>
-        <img src ={logo} alt ='لوگو شادناک' className = 'logo' ></img>
-        <div className = 'break'></div>
-        <Link  to ='/'  className = 'navlink'> 
-        </Link>
-        <Link  to = '/فروشگاه-شادناک' className = 'navlink' > 
-        فروشگاه آنلاین
-        </Link>
-        <Link  to = 'تماس-باما'  className = 'navlink'> 
-        تماس با ما
-        </Link>
-        <Link  to = '/درباره-ما' className = 'navlink'> 
-        درباره ما
-        </Link>
-        <Link  to = '/حساب-کاربری' className = 'navlink'> 
-        حساب کاربری
-        </Link>
-        <Link  to = '/سبد-خرید' className ='navlink' > 
-        سبدخرید
-        </Link>
-        <Link  to = '/مقالات' className ='navlink' > 
-        مقالات
-        </Link>
-        </header>
-      
+    <Link  to ='/'   className = 'navlink'> 
+    </Link>
+    <Link  to = '/فروشگاه-شادناک'   className = 'navlink' > 
+    فروشگاه آنلاین
+    </Link>
+    <Link  to = 'تماس-باما'  className = 'navlink' > 
+    تماس با ما
+    </Link>
+    <Link  to = '/درباره-ما' className = 'navlink'  > 
+    درباره ما
+    </Link>
+    <Link  to = '/حساب-کاربری'  className = 'navlink' > 
+    حساب کاربری
+    </Link>
+    <Link  to = '/سبد-خرید'   className = 'navlink' > 
+    سبدخرید
+    </Link>
+    <Link  to = '/مقالات'   className = 'navlink' > 
+    مقالات
+    </Link>  
+    </header>)
+    return (  
+      <>
+       
+        {isMobile ? <MobileNav> {open ? <ShowMenu>{content}</ShowMenu>:content}  </MobileNav> :
+         <DesktopNav> {content}</DesktopNav>}
+        
+      </>
+       
     )
 }
 export default Header
---
+++ client/shadnak/src/component/Pages/firstpage/styles.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components'
+export const DesktopNav = styled.section`.container{
+    display: flex;
+    height: 20%;
+    width: 100%;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;}
+ 
+
+
+.navlink{
+    color :yellow;
+    text-decoration: none;
+    &:hover{
+        color: black;
+        text-decoration-line: underline;
+        text-decoration-color: black ;
+        text-underline-offset: 1em;
+    }
+    
+}
+
+
+.logo{
+   height:10% ;
+   width:10% ;
+   padding: 2% 2% 2% 0%;
+}
+.break{
+    width: 88%;
+}
+
+.icon{
+  display: none;
+}
+`
+export  const MobileNav = styled.section`.container{
+    display: flex;
+    height: 20%;
+    width: 100%;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;}
+ 
+
+
+.navlink{
+    display : none;
+    color :yellow;
+    text-decoration: none;
+    &:hover{
+        color: black;
+        text-decoration-line: underline;
+        text-decoration-color: black ;
+        text-underline-offset: 1em;
+    }
+    
+}
+.logo{
+    width:50% ;
+    height:auto ;
+ }
+ .break{
+     width: 88%;
+ }
+ 
+ .icon{
+   display: inline;
+ }
+ .icon{
+    width: 10%;
+    padding: 5%;
+    height: auto;
+    display: inline;
+}
+ 
+ `
+export const ShowMenu = styled.section`
+.container{
+    flex-direction : column;
+}
+.navlink{
+     display : inline;
+     padding-top :5%;
+}
+
+`
