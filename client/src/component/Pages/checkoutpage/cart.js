ac4c9371ff5cf2cd0ce44d67840566d9bca0af01
saeed <mohammadsaeedmedia@gmail.com>
2021-09-07 19:47:54 +0430

    cart section in checkout

--- client/src/component/Pages/checkoutpage/cart.js
+++ client/src/component/Pages/checkoutpage/cart.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import {TiShoppingCart} from 'react-icons/ti'
+import { IconContext } from "react-icons";
+import { useSelector } from 'react-redux';
+import { useGetproductQuery } from '../../../services/shadnakapi'
 
 const Cart = () => {
+  const {data,isSuccess} = useGetproductQuery('all')
+  const product = useSelector(state => state.counter.amount)
+  const total = useSelector(state => state.counter.total)
+  let cart = []
+  if(isSuccess){
+  for(let x in product){
+    if (product[x] > 0){
+     for (let y of data){
+       if(y._id === x ){
+         cart.push({name : y.name , price : y.price , amount: product[x]})
+       }
+     }
+     }
+    }
+  }
     return (
-        <div>
-          سلام  
+      <>
+        <div checkout-page-cart-container>
+          <div>
+           <IconContext.Provider value = {{size : '1.5vmax'}}>
+             <TiShoppingCart/>
+            </IconContext.Provider>
+           <span style = {{fontSize:'1.5vmax'}}>خلاصه سبد خرید</span>
+          </div>
+          <hr ></hr>
+
+          {isSuccess && cart.map(item=>{
+            const amount = item.amount
+            const price = item.price.onSale? Number(item.price.onSale) * Number(item.amount) + ' تومان':Number(item.price.original)*Number(item.amount)
+                    + ' تومان'
+          return (<div style = {{textAlign : 'center' , fontSize :'1.3vmax'}}> 
+                    <span>{item.name}</span>
+                    <span style = {{marginRight:'2%'}}>
+                    &#10005;
+                    {amount}
+                    </span>
+                   <div style = {{display:'inline' ,marginRight:'10%'}}> <span >{price}</span></div>
+                 </div>)
+          })}
+          {isSuccess && 
+          cart.length > 0 && (
+          <div style = {{fontSize : '1.3vmax'}}><hr></hr>
+            <span> مجموع خرید :  </span>
+            <span>{total} تومان</span>
+            <span style = {{marginRight:'10%'}}> کد تخفیف:</span>
+            <input style = {{marginRight:'5%' , fontSize : '1.3vmax'}}></input>
+          </div>
+          )
+          }
         </div>
+        </>
     )
 }
 
--- client/src/component/Pages/checkoutpage/checkoutpage.js
+++ client/src/component/Pages/checkoutpage/checkoutpage.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Form from './form'
 import Cart from './cart'
 import './style.css'
+import Header from './../firstpage/header.js'
 const Checkoutpage = () => {
     return (
+        <>
+        <Header logosize = {'20%'} breakwidth = {'55%'}/>
         <div className = 'checkout-page-container'>
             <div className = 'checkout-page-form'>
                 <Form/>
@@ -14,6 +17,7 @@ const Checkoutpage = () => {
 
             
         </div>
+        </>
     )
 }
 
