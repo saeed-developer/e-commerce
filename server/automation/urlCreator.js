99fc926e7e58f2bc3208e6d8b96c2580c22f91e1
saeed <mohammadsaeedmedia@gmail.com>
2021-09-21 18:47:37 +0430

    get shoppage name from database

--- server/automation/urlCreator.js
+++ server/automation/urlCreator.js
@@ -2,12 +2,11 @@ import urlModel from './../models/url.js'
 import productModel from "./../models/products.js";
  export const  urlCreator = async(req,res)=>{
     try{
-    const productUrl = await urlModel.find()
-   
+    const productUrl = await urlModel.find({pathName : 'ProductPage'}) 
     if (productUrl.length === 0){
-        
         const product = await productModel.find().select('_id name') 
-        const urlList =  nameToUrl(product)
+        const path= await urlModel.find({pathName : 'ShopPage'}).select('path')
+        const urlList =  nameToUrl(product,path[0].path)
         await urlModel.insertMany(urlList)
     }
     else  {
@@ -19,14 +18,17 @@ catch(err){
     res.json({message : err.message})
 }
 }
-function nameToUrl(items){
+function nameToUrl(items,basePath){
     let paths = []
     for(let x of items){
         const name = x.name.replace(  /[' ' , (]/g,'-').slice(0,-1)          
-        const path = `/فروشگاه-شادناک/${name}`
+        const path = basePath + '/' + name
+        const pathName = 'ProductPage'
         paths.push({
             productId : x._id,
-            path:path})
+            path:path,
+            pathName : pathName 
+        })
     }
    return paths
 }
