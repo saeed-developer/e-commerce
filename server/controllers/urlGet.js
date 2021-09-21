77d70da5100111b364792eee8d966f8008dacccd
saeed <mohammadsaeedmedia@gmail.com>
2021-09-21 18:48:19 +0430

    change some query

--- server/controllers/urlGet.js
+++ server/controllers/urlGet.js
@@ -1,16 +1,18 @@
 import urlModel from "./../models/url.js"
 import productModel from "../models/products.js"
 export const getUrl = async(req,res)=>{
-   const {key} = req.query
+   const {key , pathName} = req.query
    try{
-        if(key === 'main'){
-        const response = await urlModel.find().select("main")
-        res.status(200).json(response)}
-        else if (key === 'product'){
-            const response = await urlModel.find().select("product")
+        if(key === 'product'){
+        const response = await urlModel.find({productName : "product"})
+        res.status(200).json(response)
+    
+    }
+        else if (key === 'main'){
+            const response = await urlModel.find({productName : "name"})
             res.status(200).json(response) 
         }
-        else if (key === 'product/name'){
+        /*else if (key === 'product/name'){
             const response = await urlModel.aggregate(
                 [
                  { $lookup:{
@@ -25,12 +27,16 @@ export const getUrl = async(req,res)=>{
                 ]    
  )
               res.status(200).json(response)
-        }
+        }*/
       
-        else {
+        else if (key == undefined) {
             const response = await urlModel.find()
             res.status(200).json(response) 
-        }       
+        }  
+        else if (key === 'pathName') {
+         const response = await urlModel.find({pathName:pathName })
+         res.status(200).json(response)
+        }   
    }
    catch(err){
        res.status(404).json({message:err.message})
