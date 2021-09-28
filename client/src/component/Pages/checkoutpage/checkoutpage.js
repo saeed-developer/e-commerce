795a1124d9b0bae66d80416bc0ed55d0b8ba851a
saeed <mohammadsaeedmedia@gmail.com>
2021-09-28 19:00:50 +0330

    use useMedia

--- client/src/component/Pages/checkoutpage/checkoutpage.js
+++ client/src/component/Pages/checkoutpage/checkoutpage.js
@@ -2,22 +2,32 @@ import React from 'react'
 import Form from './form'
 import Cart from './cart'
 import './style.css'
+import { useMedia } from 'react-use'
 import Header from './../firstpage/header.js'
 //باید چک کنم اگر موبایل بود اول کارت قرار بگیرد بعد فرم
 const Checkoutpage = () => {
-    return (
-        <>
-        <Header logosize = {'20%'} breakwidth = {'55%'}/>
-        <div className = 'checkout-page-container'>
+    const desktop = (<div className = 'checkout-page-container'>
             <div className = 'checkout-page-form'>
                 <Form/>
             </div>
             <div className = 'checkout-page-cart'>
                 <Cart/>
+            </div> 
+        </div>)
+    const mobile = 
+    (<div className = 'checkout-page-container'>
+            <div className = 'checkout-page-cart'>
+                <Cart/>
             </div>
-
-            
-        </div>
+            <div className = 'checkout-page-form'>
+                <Form/>
+            </div> 
+        </div>)
+    const isMobile = useMedia('(max-width: 768px)')
+    return (
+        <>
+        <Header logosize = {'20%'} breakwidth = {'55%'}/>
+        {isMobile ? mobile : desktop}
         </>
     )
 }
