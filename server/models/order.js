02c085c6f59780193ab9a2d5b46e0ec47a9286f3
saeed <mohammadsaeedmedia@gmail.com>
2021-09-29 17:37:28 +0330

    create model for order

---
+++ server/models/order.js
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose'
+const orderSchema = mongoose.Schema(
+{
+name : {
+    type : String,
+    required:true,
+    trim:true
+},
+province : {
+    type : String,
+    required:true,
+    trim:true
+},
+city : {
+    type : String,
+    required:true,
+    trim:true
+},
+address:{
+    type : String,
+    required:true,
+    trim:true
+},
+postalCode :{
+    type : Number,
+    required:true
+},
+phone : {
+    type : Number,
+    required:true
+},
+email : String,
+explination: {
+    type : String,
+    trim:true
+},
+date : {
+    type : Date,
+    default:new Date()
+},
+orderNumber : {
+    type : Number,
+    required:true
+},
+discount:{
+ isCode : {type : Boolean , required: true},
+ code : {type : String , trim : true}
+},
+product : {
+    type : Array,
+    required : true
+},
+total: {
+    type : Number,
+    required : true
+},
+paid:{
+    isPaid : {type : Boolean , required : true},
+    code : {type : String , trim : true}
+}
+}
+)
+const orderModel = mongoose.model('orderModel',orderSchema)
+export default orderModel
