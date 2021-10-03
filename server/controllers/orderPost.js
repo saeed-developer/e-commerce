3e04e8078713417db02cc73f2e585047d772fed2
saeed <mohammadsaeedmedia@gmail.com>
2021-10-03 18:20:19 +0330

    add some line

--- server/controllers/orderPost.js
+++ server/controllers/orderPost.js
@@ -1,8 +1,17 @@
 import orderModel from "../models/order.js";
+import {app} from './../app.js'
 export const postOrder = async(req,res)=>{
+    let orderNubmer;
+    let firstRequest = app.get('firstReq')
+    if (firstRequest  === true) {
+        app.set('firstReq',false)
+        const Number = await orderModel.find().sort({_id : 1}).limit(1).select('orderNumber')
+        if (Number.length  < 1) orderNubmer = 500
+        else orderNubmer = Number
+    }
     const body = req.body
     try{
-     
+     //if ordersaved to db increment orderNumber by 1
     }
     catch(err){
         res.status(404).json({message : err.message})
