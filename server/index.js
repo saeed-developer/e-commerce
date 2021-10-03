ab406b129cf6ec46b56d5c81506fb2c01669478c
saeed <mohammadsaeedmedia@gmail.com>
2021-10-03 18:20:03 +0330

    global app var

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-import express, { urlencoded } from 'express'
+import express ,{ urlencoded } from 'express'
+import {app} from './app.js'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import router from './routes/router.js'
 import { config } from 'dotenv' 
 import {urlCreator} from './scripts/urlCreator.js'
 config({path: './config/.env'})
-const app = express()
 app.use(express.urlencoded({limit : '1Mb' , extended : true}))
 app.use (express.json({limit : '1Mb' , extended : true}))
 app.use(cors())
 app.use('/',router)
+app.set('firstReq',true)
 const DB = process.env.DB 
 const Port = process.env.PORT || 3000
 mongoose.connect(DB, {useNewUrlParser : true , useUnifiedTopology : true})
