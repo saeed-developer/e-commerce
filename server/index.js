6bbf65589f1d3d69720cda70b4eac4a286ba2575
saeed <mohammadsaeedmedia@gmail.com>
2021-10-06 01:08:45 +0330

    add subscriber

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 import router from './routes/router.js'
 import { config } from 'dotenv' 
 import {urlCreator} from './scripts/urlCreator.js'
+import {subscribers} from './subscribers/subscriber.js'
+import {newOrder} from './subscribers/sms.js'
 config({path: './config/.env'})
 app.use(express.urlencoded({limit : '1Mb' , extended : true}))
 app.use (express.json({limit : '1Mb' , extended : true}))
@@ -15,6 +17,7 @@ const DB = process.env.DB
 const Port = process.env.PORT || 3000
 mongoose.connect(DB, {useNewUrlParser : true , useUnifiedTopology : true})
 .then(()=> app.listen(Port ,
+    subscribers,
     urlCreator(), 
     console.log(`connected to database and server is listening on port ${Port}`)))
 .catch((err)=> console.log(err.message))
