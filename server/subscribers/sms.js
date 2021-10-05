8e75272f1a5f96fb97f1d147d1b69a4867999ab5
saeed <mohammadsaeedmedia@gmail.com>
2021-10-06 01:10:58 +0330

    send sms after order

---
+++ server/subscribers/sms.js
@@ -0,0 +1,22 @@
+import event from 'events'
+import axios from 'axios'
+import {writeFile} from 'fs'
+export const newOrder = new event()
+newOrder.on('newOrder', (order)=>{
+    writeFile('./records/smsRecord.txt',`\nsmsSent`,{flag : 'a'},(err)=>{
+        if (err)console.error(err)
+    })
+    axios.post(process.env.smsUrl,{
+        op : "send",
+        uname : process.env.smsUsername,
+        pass:  process.env.smsPassword,
+        message : `سفارش جدید دارید
+        مبلغ :${order.total}`,
+        from: process.env.smsFrom,
+        to : [process.env.smsTo],
+    })
+    .then(()=>  writeFile('smsRecord.txt',`\nsmsSent`,{flag : 'a'}),(err)=>{
+       if(err) console.error(err)
+    } , () =>  writeFile('smsRecord.txt',`\nsmsNotsent`,{flag : 'a'}),(err)=>{
+        if(err) console.error(err)})
+})
