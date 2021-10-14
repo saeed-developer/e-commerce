d2cbd3b718b7753e141152b04a60362368c49c07
saeed <mohammadsaeedmedia@gmail.com>
2021-10-14 03:50:56 +0330

    pass html as prop to component

--- client/src/component/Pages/firstpage/firstpage.js
+++ client/src/component/Pages/firstpage/firstpage.js
@@ -4,16 +4,24 @@ import {SlideShow} from './slideshow.js'
 import {PrimaryContent , SeconderyContent} from './content.js'
 import './styles.css'
 import Footer from './footer.js'
+import { useGetcontentQuery } from '../../../services/shadnakapi.js'
 
 const FirstPage = ()=>{
-   
+   const {data,isError,refetch,isSuccess} = useGetcontentQuery({key : ''})
+  isError && setInterval(()=>{
+      refetch()
+  },1000)
+  console.log(data)
     return(
         <>
+       
         <Header />
-        <SlideShow/>
+        <SlideShow/> 
         <PrimaryContent/>
-        <SeconderyContent/>
-        <Footer/>
+        { isSuccess && data.map((item)=>{
+            if(item.section === 'content') return <SeconderyContent  content  = {<div dangerouslySetInnerHTML={{__html: item.content}} />}/>
+            if(item.section === 'footer') return <Footer content  = {<div dangerouslySetInnerHTML={{__html: item.content}} />}/>
+        })}
         </>
     )
 }
