e30775e5dbb8c1e137a9ceab9e4f2392c273c920
saeed <mohammadsaeedmedia@gmail.com>
2021-10-24 16:23:15 +0330

    delete comment

--- client/src/component/Pages/checkoutpage/checkoutpage.js
+++ client/src/component/Pages/checkoutpage/checkoutpage.js
@@ -4,7 +4,6 @@ import Cart from './cart'
 import './style.css'
 import { useMedia } from 'react-use'
 import Header from './../firstpage/header.js'
-//باید چک کنم اگر موبایل بود اول کارت قرار بگیرد بعد فرم
 const Checkoutpage = () => {
     const desktop = (<div className = 'checkout-page-container'>
             <div className = 'checkout-page-form'>
