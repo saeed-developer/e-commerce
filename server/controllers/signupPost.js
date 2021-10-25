2b4ab0cba1e41c9e509d690817998b6c75478900
saeed <mohammadsaeedmedia@gmail.com>
2021-10-25 17:52:44 +0330

    use bcrypt

--- server/controllers/signupPost.js
+++ server/controllers/signupPost.js
@@ -1,19 +1,22 @@
 import userModel from "../models/users.js";
 import User from "../models/users.js";
 import { validationResult } from "express-validator"
+import bcrypt from 'bcrypt'
 const signUp = async(req,res)=>{
    const {email} = req.body
    const {password} = req.body
-  //next 
-  //I should hash the password 
+   const saltRound = 12
    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() }); 
     }
-   const newUser = new userModel(req.body)
-    try{
+   
+    try{  
       const userExist = await User.findOne({email : email})
       if(userExist == null){
+      const hash = await bcrypt.hash(password,saltRound) 
+      console.log(hash)
+      const newUser = new userModel({email : email,password:hash}) 
       await newUser.save()
       res.json({message : 'شما با موفقیت ثبت نام کردید برای ورود به حساب کاربری از قسمت ورود اقدام نمایید'})
     }
