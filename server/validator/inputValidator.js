23b2bbc4fd8463af7e8221d6fa216d13354ee5c0
saeed <mohammadsaeedmedia@gmail.com>
2021-10-25 15:49:32 +0330

    create validator for user

--- server/validator/inputValidator.js
+++ server/validator/inputValidator.js
@@ -7,4 +7,6 @@ body('name').isLength({max : 50}).withMessage('اسم نامعتبر') ,
     body('email').isEmail().withMessage('ایمیلی که وارد کردید نامعتبر است'),
 ]
 export const discountCodeValidator = [query('key').trim().isLength({min : 6}).isAlphanumeric()]
-export const orderValidator = []
+export const userValidator = [body('email').isEmail().withMessage('ایمیلی که وارد کرده اید نامعتبراست'),
+body('password').isStrongPassword({ minLength: 6, minLowercase: 0, minUppercase: 0, minNumbers: 1, minSymbols: 0,}).withMessage('پسورد باید حداقل 6 کارکتر و شامل  اعداد و حروف انگلیسی باشد')
+]
