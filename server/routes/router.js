3e996dd17ebd79f3aee6b4c288f277298b904e2e
saeed <mohammadsaeedmedia@gmail.com>
2021-10-26 16:49:13 +0330

    add login module

--- server/routes/router.js
+++ server/routes/router.js
@@ -12,6 +12,7 @@ import { getCities } from '../controllers/citiesGet.js'
 import {postOrder} from '../controllers/orderPost.js'
 import { postContent } from '../controllers/contentPost.js'
 import { getContent } from '../controllers/contentGet.js'
+import { login } from '../controllers/loginPost.js'
 import signUp from '../controllers/signupPost.js'
 import ratelimit from 'express-rate-limit'
 
@@ -35,4 +36,5 @@ router.post('/post-order',postOrder)
 router.post('/post-content',postContent)
 router.get('/get-content',getContent)
 router.post('/post-signup',userValidator,signUp)
+router.post('/post-login',login)
 export default router
