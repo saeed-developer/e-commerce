4faab29521b5ea39806e3c1a0bedd7655b6fa50d
saeed <mohammadsaeedmedia@gmail.com>
2021-10-27 22:01:30 +0330

    remove console

--- client/src/component/Pages/firstpage/footer.js
+++ client/src/component/Pages/firstpage/footer.js
@@ -3,7 +3,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Instagram from './../../../images/instagram.svg'
 const Footer = ({content}) => {
- console.log(content)
      return(
          <>
           {content}
