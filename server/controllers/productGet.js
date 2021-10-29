d5c5e50d9384190d8a695f7e52963b878078676c
saeed <mohammadsaeedmedia@gmail.com>
2021-10-29 16:42:12 +0330

    delete comment

--- server/controllers/productGet.js
+++ server/controllers/productGet.js
@@ -24,21 +24,9 @@ catch(err){
     }
 
 }
-/*
-export const getProduct = (req,res)=>{
-const query = req.query.number
-console.log(query)
-productModel.find().select('name price').exec((err,doc)=>{
-    if(err){
-        res.status(500).send(err)
-    }
-    res.status(200).json(doc)
-})}  this is another approach
- */
 export const getProductImage = async(req,res)=>{
-      try{
-        const {id} = req.query
-        
+const {id} = req.query  
+      try{   
         if(id === 'all'){
             const file = await promises.readdir ('./images/shop-page')
             res.status(200).send(file)
@@ -47,10 +35,7 @@ export const getProductImage = async(req,res)=>{
           await asyncpipe(
             createReadStream(`./images/shop-page/${id}`)
             ,res
-          )
-          
-                          }     
-         }
+          )}}
     catch(err){
       res.json({message:err.message})
               }
