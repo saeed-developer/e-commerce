bd4f5b42a661f4c180fece895c3e2e589a30ac5f
saeed <mohammadsaeedmedia@gmail.com>
2021-10-29 16:41:34 +0330

    delete comment and import

--- server/controllers/urlGet.js
+++ server/controllers/urlGet.js
@@ -1,34 +1,15 @@
 import urlModel from "./../models/url.js"
-import productModel from "../models/products.js"
 export const getUrl = async(req,res)=>{
    const {key , pathName} = req.query
    try{
         if(key === 'product'){
         const response = await urlModel.find({productName : "product"})
         res.status(200).json(response)
-    
     }
         else if (key === 'main'){
             const response = await urlModel.find({productName : "name"})
             res.status(200).json(response) 
         }
-        /*else if (key === 'product/name'){
-            const response = await urlModel.aggregate(
-                [
-                 { $lookup:{
-                    from : 'productmodels',
-                    localField:'productId',
-                    foreignField:'_id',
-                    as:'urlname'
-
-                    }},
-                  
-                
-                ]    
- )
-              res.status(200).json(response)
-        }*/
-      
         else if (key == undefined) {
             const response = await urlModel.find()
             res.status(200).json(response) 
